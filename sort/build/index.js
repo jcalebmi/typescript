9f1978734e24f2e1a8d8de2e5a59854668bc056e
Caleb Iuliano <calebiuliano@gmail.com>
2021-08-08 22:53:49 -0400

    created sorter for array, string & linked list

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,11 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Sorter_1 = require("./Sorter");
+var NumbersCollection_1 = require("./NumbersCollection");
 var CharactersCollection_1 = require("./CharactersCollection");
-// const numbersCollection = new NumbersCollection([50, 3, -5, 0])
-// const sorter = new Sorter(numbersCollection)
+var LinkedList_1 = require("./LinkedList");
+var numbersCollection = new NumbersCollection_1.NumbersCollection([50, 3, -5, 0]);
+numbersCollection.sort();
 var charactersCollection = new CharactersCollection_1.CharactersCollection('Xaayz');
-var sorter = new Sorter_1.Sorter(charactersCollection);
-sorter.sort();
-// console.log(numbersCollection.data);
+charactersCollection.sort();
+var linkedList = new LinkedList_1.LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.sort();
+linkedList.print();
+console.log(numbersCollection.data);
 console.log(charactersCollection.data);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,14 +1,21 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-// const numbersCollection = new NumbersCollection([50, 3, -5, 0])
-// const sorter = new Sorter(numbersCollection)
+const numbersCollection = new NumbersCollection([50, 3, -5, 0])
+numbersCollection.sort();
 
 const charactersCollection = new CharactersCollection('Xaayz');
-const sorter = new Sorter(charactersCollection);
+charactersCollection.sort();
 
-sorter.sort();
-// console.log(numbersCollection.data);
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.sort();
 
+linkedList.print();
+console.log(numbersCollection.data);
 console.log(charactersCollection.data);
