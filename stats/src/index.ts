4896d1d3e03f3d38058c18bcce411dc70b717340
Caleb Iuliano <calebiuliano@gmail.com>
2021-08-10 18:40:08 -0400

    implemented compisition

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,17 +1,12 @@
 import { MatchReader } from "./MatchReader";
-import { MatchResult } from "./MatchResult";
+import { Summary } from "./Summary";
 
-const reader = new MatchReader('football.csv');
-reader.read();
+const matchReader = MatchReader.fromCsv('football.csv');
+matchReader.load();
 
-let manUnitedWins = 0;
+// const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport());
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
+summary.buildAndPrintReport(matchReader.matches);
 
-for (let match of reader.data) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
 
-console.log(`Man United won ${manUnitedWins} games`);
+
